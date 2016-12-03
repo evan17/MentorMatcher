2c8b4397fd5ab0ea0ceac7acbde09fe75f2acf6f
evan17 <mercuryly@gmail.com>
2016-12-03 11:37:10 -0500

    Updated comment and slot class; Added firebase support

--- app/src/main/java/edu/umich/mentormatcher/Comment.java
+++ app/src/main/java/edu/umich/mentormatcher/Comment.java
@@ -1,24 +1,62 @@
 package edu.umich.mentormatcher;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Created by Zhihao on 11/28/2016.
  */
 
 public class Comment {
-    private int commentId;
-    private String commentText;
+    private static final String DB_NAME_COMMENT = "comments";
+    private String commentId;
+    private int receiverUid;
+    private String text;
     private int rating;
     private boolean isMentorComment;
+    private Date createTime;
 
-    private Comment() {
-        // Disable default constructor
+    protected Comment() {    } // Protect default constructor
+
+    private Comment(int receiverUid, int rating, String text, Date createTime, boolean isMentorComment) {
+        this.receiverUid = receiverUid;
+        this.rating = rating;
+        this.text = text;
+        this.isMentorComment = isMentorComment;
+        this.createTime = createTime;
+        this.commentId = generateNewCommentId();
     }
 
-    public Comment(int rating, String commentText) {
-
+    public boolean modifyComment(int rating, String text) {
+        this.rating = rating;
+        this.text = text;
+        return true;
     }
 
-    public Comment getCommentById(int commentId) {
-        return null;
+    public boolean saveToFirebase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference commentRef = database.getReference(DB_NAME_COMMENT);
+        if(commentRef == null)
+            return false;
+
+        commentRef.child(Integer.toString(receiverUid)).child(commentId).setValue(this);
+        return true;
+    }
+
+    public static DatabaseReference getCommentDBRefFromFirebase(String commentId) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference commentRef = database.getReference(DB_NAME_COMMENT).child(commentId.split("-")[0]);
+
+        return commentRef;
+    }
+
+    private String generateNewCommentId() {
+        if(this.receiverUid == 0)
+            return null;
+        Random r = new Random();
+        return Integer.toString(this.receiverUid) + "-" + this.createTime + Integer.toString(r.nextInt());
     }
 }
--- app/src/main/java/edu/umich/mentormatcher/Slot.java
+++ app/src/main/java/edu/umich/mentormatcher/Slot.java
@@ -1,25 +1,82 @@
 package edu.umich.mentormatcher;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Date;
+import java.util.Random;
 
 /**
  * Created by Zhihao on 11/28/2016.
  */
 
 public class Slot {
-    private int slotId;
+    private static final String DB_NAME_SLOT = "slots";
+    private String slotId;
     private Date startTime;
     private Date endTime;
-    private Mentor mentor;
+    private int mentorUid;
     private String typeOfService;
 
     private boolean isBooked;
     private boolean isConfirmed;
-    private Mentee mentee;
+    private int menteeUid;
 
-    public static Slot getSlotById(int slotId) {
-        return null;
+    protected Slot() {} // Protect default constructor
+    public Slot(Date startTime, Date endTime, int mentorUid, String typeOfService) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.mentorUid = mentorUid;
+        this.typeOfService = typeOfService;
+        this.isBooked = false;
+        this.isConfirmed = false;
+        this.menteeUid = 0;
+        this.slotId = generateNewSlotId();
     }
 
+    public boolean setAsBooked(int menteeUid) {
+        if(this.isBooked)
+            return false;
 
+        this.isBooked = true;
+        this.menteeUid = menteeUid;
+        return true;
+    }
+
+    public boolean setAsConfirmed(int mentorUid) {
+        if(this.isBooked && mentorUid == this.mentorUid) {
+            this.isConfirmed = true;
+            return true;
+        }
+        return false;
+    }
+
+    public static DatabaseReference getSlotDBRefFromFirebase(String slotId) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference slotRef = database.getReference(DB_NAME_SLOT).child(slotId.split("-")[0]);
+
+        return slotRef;
+    }
+
+    private boolean saveToFirebase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference slotRef = database.getReference(DB_NAME_SLOT);
+        if(slotRef == null)
+            return false;
+
+        slotRef.child(Integer.toString(mentorUid)).child(slotId).setValue(this);
+        return true;
+    }
+
+    private String generateNewSlotId() {
+        if(this.mentorUid == 0)
+            return null;
+        Random r = new Random();
+        return Integer.toString(this.mentorUid) + "-" + this.startTime.toString() + Integer.toString(r.nextInt());
+    }
 }
