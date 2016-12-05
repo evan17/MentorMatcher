e363aa5791eb3c83e7024c2630a8c9b250b7e09d
Elizabeth <erlcroix@umich.edu>
2016-12-05 10:20:42 -0500

    Lizzie re-writing registration code

--- app/src/main/java/edu/umich/mentormatcher/Registration.java
+++ app/src/main/java/edu/umich/mentormatcher/Registration.java
@@ -2,6 +2,16 @@ package edu.umich.mentormatcher;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Spinner;
+import java.util.List;
+import android.widget.AdapterView.OnItemSelectedListener;
+import java.util.ArrayList;
 
 public class Registration extends Activity {
 
