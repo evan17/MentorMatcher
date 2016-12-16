10ddcca7961f931f101757ad2faadcec924dfc69
kgall_000 <k.gallagher555@gmail.com>
2016-12-16 14:50:46 -0500

    Registration Updates

--- app/src/main/java/edu/umich/mentormatcher/Registration.java
+++ app/src/main/java/edu/umich/mentormatcher/Registration.java
@@ -34,9 +34,10 @@ public class Registration extends Activity implements View.OnClickListener, OnIt
 
     private Button buttonC;
     private Button buttonBecomeMentor;
+    private EditText editTextName;
+    private Spinner ConfirmAspiration;
     private EditText editTextEmail;
     private EditText editTextPassword;
-    private Spinner ConfirmAspiration;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -47,16 +48,15 @@ public class Registration extends Activity implements View.OnClickListener, OnIt
         setContentView(R.layout.activity_registration);
 
         //Link to UI
-        buttonC = (Button) findViewById(R.id.buttonC);
-        buttonBecomeMentor = (Button) findViewById(R.id.buttonBecomeMentor);
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonC = (Button)findViewById(R.id.buttonC);
+        buttonBecomeMentor = (Button)findViewById(R.id.buttonBecomeMentor);
+        editTextName = (EditText) findViewById(R.id.editTextName);
         ConfirmAspiration = (Spinner) findViewById(R.id.ConfirmAspiration);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+
 
         buttonC.setOnClickListener(this);
         buttonBecomeMentor.setOnClickListener(this);
-        editTextEmail.setOnClickListener(this);
-        editTextPassword.setOnClickListener(this);
         ConfirmAspiration.setOnItemSelectedListener(this);
 
         //Add Firebase Auth here
@@ -75,20 +75,7 @@ public class Registration extends Activity implements View.OnClickListener, OnIt
                 }
             }
         };
-    
-        }
-        @Override
-        public void onStart(){
-            super.onStart();
-            mAuth.addAuthStateListener(mAuthListener);
-        }
 
-        @Override
-        public void onStop(){
-            super.onStop();
-            if (mAuthListener !=null){
-                mAuth.removeAuthStateListener(mAuthListener);
-            }
 
         //Creating list of items in the spinner
         List<String> categories = new ArrayList<String>();
@@ -108,26 +95,41 @@ public class Registration extends Activity implements View.OnClickListener, OnIt
         //attaching data adapter to spinner
         ConfirmAspiration.setAdapter(adapter);
 
+
         //Code that gets the selected text from the selected item
 
         String selectedText = (String) ConfirmAspiration.getSelectedItem();
 
+    }
+//End of On Create!
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     private void findViewById(Button buttonC) {
     }
 
     public void onClick (View view) {
-
-        String email = editTextEmail.getText().toString();
-        String password = editTextPassword.getText().toString();
-
-        User user= new User (email,password);
+/*
+        String name = editTextName.getText().toString();
+        String email = mAuth.getCurrentUser().getEmail();
+        User user= new User (email,  );
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference dataUser = database.getReference("Users");
 
         DatabaseReference dataNewUser = dataUser.push();
-        dataNewUser.setValue(user);
+        dataNewUser.setValue(user);*/
 
         //Also this page does not capture two more elements of the User class which are name and uid
 
@@ -164,12 +166,12 @@ public class Registration extends Activity implements View.OnClickListener, OnIt
                                 long id) {
         String careerAspiration = parent.getItemAtPosition(1).toString();
 
-        User user= new User (careerAspiration);
+        /*User user= new User (careerAspiration);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference dataUser = database.getReference("Users");
 
         DatabaseReference dataNewUser = dataUser.push();
-        dataNewUser.setValue(user);
+        dataNewUser.setValue(user); */
 
         // Showing selected spinner item
         Toast.makeText(parent.getContext(), "Selected: " + careerAspiration, Toast.LENGTH_SHORT).show();
